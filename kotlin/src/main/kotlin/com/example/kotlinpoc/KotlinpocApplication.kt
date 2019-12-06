47f0f91d587f840b2afd02b55f6673bef1fcd4a7
Kimie Nakahara <kimie@hireup.com.au>
2019-12-06 13:14:22 +1100

    initial import

---
+++ kotlin/src/main/kotlin/com/example/kotlinpoc/KotlinpocApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinpocApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinpocApplication>(*args)
+}
