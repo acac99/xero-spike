5e957f4975b67e217b4993e5a144f7ff5dc13fa6
Kimie Nakahara <kimie@hireup.com.au>
2019-12-09 12:00:13 +1100

    added integration tests

--- kotlin/src/main/kotlin/com/example/kotlinpoc/web/InvoiceController.kt
+++ kotlin/src/main/kotlin/com/example/kotlinpoc/web/InvoiceController.kt
@@ -3,7 +3,9 @@ package com.example.kotlinpoc.web
 import com.example.kotlinpoc.persistence.Invoice
 import com.example.kotlinpoc.persistence.InvoiceRepository
 import com.example.kotlinpoc.persistence.LineItem
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 
 @RestController
@@ -13,7 +15,9 @@ class InvoiceController(private val repository: InvoiceRepository) {
     fun findAll() = repository.findAll()
 
     @GetMapping("/invoice/{id}")
-    fun findOne(@PathVariable id: UUID) = repository.findById(id);
+    fun findOne(@PathVariable id: UUID): ResponseEntity<Invoice> = repository.findById(id).map { invoice ->
+        ResponseEntity.ok(invoice)
+    }.orElse(ResponseEntity.notFound().build())
 
     @PostMapping("/invoice")
     fun postCustomer(@RequestBody newInvoice: NewInvoice): Invoice {
--- kotlin/src/test/kotlin/com/example/kotlinpoc/web/InvoiceControllerIntegrationTest.kt
+++ kotlin/src/test/kotlin/com/example/kotlinpoc/web/InvoiceControllerIntegrationTest.kt
@@ -1,24 +1,71 @@
 package com.example.kotlinpoc.web
 
+import com.example.kotlinpoc.persistence.Invoice
+import com.example.kotlinpoc.persistence.InvoiceRepository
+import com.example.kotlinpoc.persistence.LineItem
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONAssert
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
+import java.math.BigDecimal
+import java.util.*
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
-
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate, @Autowired val invoiceRepository: InvoiceRepository) {
 
 
     @Test
     fun `Expect GET invoice to return invoices`() {
+        //when
+        val entity = restTemplate.getForEntity<String>("/invoice")
 
-        val entity = restTemplate.getForEntity<String>("/")
+        //then
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        JSONAssert.assertEquals(entity.body, "[]", true)
+
+    }
+
+    @Test
+    fun `Expect GET invoice{id} to return 404 if invoice not found`() {
+        //given
+        val params = mapOf(Pair("id", UUID.randomUUID()))
+
+        //when
+        val entity = restTemplate.getForEntity<String>("/invoice/{id}", params)
+
+        //then
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+    }
+
+
+    @Test
+    fun `Expect GET invoice{id} to return invoice`() {
+        //given
+        val invoice = Invoice(
+                status = "pending",
+                amountDue = BigDecimal("120.00"),
+                lineItems = listOf(LineItem(
+                        description = "bla",
+                        quantity = 2.0,
+                        unitAmount = BigDecimal("60"),
+                        lineAmount = BigDecimal("120"),
+                        invoice = null
+                ))
+        )
+        val savedInvoice = invoiceRepository.save(invoice)
+        val params = mapOf(Pair("id", savedInvoice.id))
+
+        //when
+        val entity = restTemplate.getForEntity<String>("/invoice/{id}", params)
+
+        //then
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+//        JSONAssert.assertEquals( "{}", entity.body, true)
     }
 
 }
